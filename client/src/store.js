aab8385f11e68cafcef4f06b3c21ee36202d1051
sc-mark-17 <sebcamacho1703@gmail.com>
2019-06-07 23:53:27 -0500

    c8

--- client/src/store.js
+++ client/src/store.js
@@ -7,11 +7,13 @@ const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
-  () => [],
+  rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    (window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()) ||
+    compose
   )
 );
 
