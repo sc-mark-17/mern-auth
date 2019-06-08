4281ff5b368f731080c823518c81826b301fc2db
sc-mark-17 <sebcamacho1703@gmail.com>
2019-06-07 23:23:59 -0500

    c6

--- server.js
+++ server.js
@@ -30,5 +30,5 @@ require("./config/passport")(passport);
   // Routes
 app.use("/api/users", users);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`:):)Server up and running on port ${port} !`));
