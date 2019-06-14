79f85b95f6a26a9cd5c45617e847ee27e9f26e6c
sc-mark-17 <sebcamacho1703@gmail.com>
2019-06-13 22:56:46 -0500

    clear

--- client/src/components/layout/LayoutNavbar.js
+++ client/src/components/layout/LayoutNavbar.js
@@ -32,20 +32,3 @@ class LayoutNavbar extends Component {
   }
 }
 export default LayoutNavbar;
-
-/*<div className="navbar-fixed">
-  <nav className="z-depth-0">
-    <div className="nav-wrapper white">
-      <Link
-        to="/"
-        style={{
-          fontFamily: "monospace"
-        }}
-        className="col s5 brand-logo center black-text"
-      >
-        <i className="material-icons">code</i>
-        MERN
-      </Link>
-    </div>
-  </nav>
-</div>*/
