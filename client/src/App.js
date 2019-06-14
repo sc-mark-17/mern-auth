150780f614e6435f8ede9f8ae86a88769a24f2eb
sc-mark-17 <sebcamacho1703@gmail.com>
2019-06-14 07:04:20 -0500

    sin .css y sin graphql

--- client/src/App.js
+++ client/src/App.js
@@ -44,6 +44,7 @@ class App extends Component {
             <Switch>
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
+            <hr/>
             <Footer />
           </div>
         </Router>
